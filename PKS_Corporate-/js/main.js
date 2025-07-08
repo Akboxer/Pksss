cbb9306fd5e5421faa6ad91331dad2c2b161784d
SourabhBishnoi100 <129992874+SourabhBishnoi100@users.noreply.github.com>
2025-07-08 18:08:27 +0530

    Minor functionality and responsiveness added to wepages

--- PKS_Corporate-/js/main.js
+++ PKS_Corporate-/js/main.js
@@ -204,3 +204,27 @@
 
 })(jQuery);
 
+
+
+
+// JavaScript for "Scroll to the top button " on every page
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // Show or hide button on scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  });
+
+  // Scroll to top when clicked
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
